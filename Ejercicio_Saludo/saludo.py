43d2f3e772ceb9197041eb65c01e1ad76cbc1212
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-01-14 13:24:59 -0300

    Operaciones básicas realizadas!

--- Ejercicio_Saludo/saludo.py
+++ Ejercicio_Saludo/saludo.py
@@ -1,3 +1,9 @@
 # MI PRIMER PROGRAMA EN PYTHON...
 
 print("Hola Mundo!")
+
+# Ejecutar código Python en VS Code:
+
+# Ejecutar el código desde VS Code es más sencillo. Debe abrir el 
+# directorio donde está almacenado su programa haciendo uso del 
+# menú File seguido de Open folder… 
---
+++ Operaciones_Basicas/sumaYMultiplicacion.py
@@ -0,0 +1,11 @@
+# REALIZA OPERACIONES ARITMÉTICAS BÁSICAS COMO SUMA Y MULTIPLICACIÓN...
+
+# OBJETIVO: ENTENDER CÓMO REALIZAR OPERACIONES MATEMÁTICAS Y USAR VARIABLES...
+
+number1 = 5
+number2 = 3
+suma = number1 + number2
+multiplicacion = number1 * number2
+
+print("Suma total: ", suma)
+print("Multiplicacion: ", multiplicacion)
