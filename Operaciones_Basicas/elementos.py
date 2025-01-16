008d93e88c79a04d44fe879c2709b22a791a1c3f
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-01-16 13:06:29 -0300

    Agregando elementos dentro de una lista!

---
+++ Operaciones_Basicas/elementos.py
@@ -0,0 +1,45 @@
+# CREANDO LISTAS Y AGREGANDO ELEMENTOS...
+
+# APRENDE A CREAR UNA LISTA Y AGREGAR ELEMENTOS A ELLA...
+
+# OBJETIVO: COMPRENDER LAS LISTAS, UNA DE LAS ESTRUCTURAS DE DATOS MÁS UTILIZADAS EN PYTHON...
+
+my_list = []
+my_list.append(1)
+my_list.append(2)
+my_list.append(3)
+my_list.append(4)
+my_list.append(5)
+my_list.append(6)
+my_list.append(7)
+my_list.append(8)
+my_list.append(9)
+my_list.append(10)
+my_list.append(11)
+my_list.append(12)
+my_list.append(13)
+my_list.append(14)
+my_list.append(15)
+my_list.append(16)
+my_list.append(17)
+my_list.append(18)
+my_list.append(19)
+my_list.append(20)
+my_list.append(21)
+my_list.append(22)
+my_list.append(23)
+my_list.append(24)
+my_list.append(25)
+my_list.append(26)
+my_list.append(27)
+my_list.append(28)
+my_list.append(29)
+my_list.append(30)
+my_list.append(31)
+my_list.append(32)
+my_list.append(33)
+print(my_list)
+
+# RESULTADO:
+
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
