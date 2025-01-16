03dc7bb3be81043228ff660c375140f5ef0df32d
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-01-16 14:05:20 -0300

    Iterando listas...

---
+++ Operaciones_Basicas/lista_iteraciones.py
@@ -0,0 +1,12 @@
+# UTILIZA UN BUCLE "for" PARA ITERAR SOBRE LOS ELEMENTOS DE UNA LISTA...
+
+# OBJETIVO: APRENDER A USAR BUCLES, FUNDAMENTALES PARA AUTOMATIZAR Y REPETIR TAREAS...
+
+animals = {"rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"}
+
+for animal in animals:
+    print("I HAVE", animal)
+
+# Cada uno de estos ejercicios te introducirá a conceptos clave de la programación en Python de una manera práctica y sencilla. 
+# No te preocupes si no entiendes todo de inmediato. 
+# La práctica constante es la clave para dominar cualquier habilidad.
